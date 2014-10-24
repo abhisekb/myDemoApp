be2aa52040610338b6de7846d1be163755143ad6
Abhisek Banerjee <abhisek_98@yahoo.com>
2014-10-25 03:35:46 +0530

    Connected angular to express

--- server/app.js
+++ server/app.js
@@ -8,6 +8,11 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+/**
+ * Route Imports
+ */
+var signup = require('./routes/signup');
+
 var app = express();
 
 
@@ -56,5 +61,10 @@ if (app.get('env') === 'production') {
         });
     });
 }
+
+/**
+ * Routes
+ */
+app.use('/signup', signup);
 
 module.exports = app;
